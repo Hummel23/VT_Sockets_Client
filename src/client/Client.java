f287f9a40c95936fd9d4ea8422856ef9b6adb110
Saba <nigistsaba@hotmail.com>
2016-11-10 23:01:56 +0100

    exercise 2 client complete

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,15 +1,18 @@
 package client;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+
 public class Client {
 	private static int SERVER_PORT = 7890;
 
-	public static void main(String[] args) {
-		BufferedReader br;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br, br2;
 		PrintWriter out;
 		ValidationService validationService = ValidationService.getInstance();
 
@@ -17,54 +20,67 @@ public class Client {
 		View view = new View();
 		view.welcome();
 
-		while(true){
+		// connect to server
+		Socket client = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
+		// System.out.println("Connected to Server...");
+
+		//for user input
+		br = new BufferedReader(new InputStreamReader(System.in));
+
+		// for receiving data from Server
+		br2 = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+		//for sending data to server
+		out = new PrintWriter(client.getOutputStream(), true);
+
+		String input;
+
+		boolean isValidInput, isConnected = true;
+		while (isConnected) {
 			try {
 
-				br = new BufferedReader(new InputStreamReader(System.in));
-				String input;
-				boolean isValidInput=false;
+				isValidInput = false;
 
-				//input and validation user input
-				do{
+				// input and validation user input
+				do {
 					input = br.readLine();
-					if(validationService.checkInput(input).equals("exit")){
+					if (validationService.checkInput(input).equals("exit")) {
 						view.goodbye();
-						return;
+						isConnected=false;
+						client.close();
+						break;
 					}
-					if(validationService.checkInput(input).equals("help")){
+					if (validationService.checkInput(input).equals("help")) {
 						view.help();
 					}
-					if(validationService.checkInput(input).equals("")){
-						isValidInput=true;
-					}else
+					if (validationService.checkInput(input).equals("")) {
+						isValidInput = true;
+					} else
 						System.out.println(validationService.checkInput(input));
-				}while(!isValidInput);
+				} while (!isValidInput);
 
+				if (isConnected) {
 
-				// connect to server
-				Socket client = new Socket(InetAddress.getLocalHost(), SERVER_PORT);
-				//				System.out.println("Connected to Server...");
+					// send data to server when input is valid
+					out.println(input);
 
-				//send data to server
-				out = new PrintWriter(client.getOutputStream(), true);
-				out.println(input);
+					//receive data from Server
+					String result = br2.readLine();
 
-
-				//receive data from Server
-				br = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				String result = br.readLine();
-				//				System.out.println(result);
-
-				//close connection
-				client.close();
-
-				//display result to user
-				view.result(result);
+					// display result to user
+					view.result(result);
+				}
 
 			} catch (Exception e) {
 				System.out.println("Connection to Server failed.");
 			}
+
 		}
+		// close connection
+		br.close();
+		br2.close();
+		out.close();
+		client.close();
 	}
 
 }
